56c84cdf246a316930314cfb18b3a50d972a1202
Odd Morten <odd.morten@sveas.org>
2011-03-28 23:40:46 +0200

    lagt til søkeboks

---
+++ TheGym/ScheduleSearchBar.cs
@@ -0,0 +1,16 @@
+using System;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+
+namespace TheGym
+{
+	public class ScheduleSearchBar : UISearchBar
+	{
+		public ScheduleSearchBar () 
+		{
+		}
+		
+		
+	}
+}
+
--- TheGym/ScheduleViewController.cs
+++ TheGym/ScheduleViewController.cs
@@ -21,9 +21,14 @@ namespace TheGym
 			
 			Title = "Gruppetimer";
 			
+			
+			ScheduleSearchBar searchBar = new ScheduleSearchBar();
+			searchBar.Frame = new RectangleF ( 0, 0, View.Frame.Width, 40 ) ;			
+			View.AddSubview( searchBar );
+	
 			ScheduleTableViewController sheduleTableViewController = new ScheduleTableViewController( this._navigationController );
-						
-			sheduleTableViewController.View.Frame = new RectangleF( 0,0,View.Frame.Width,View.Frame.Height );
+			
+			sheduleTableViewController.View.Frame = new RectangleF( 0, searchBar.Frame.Height, View.Frame.Width, View.Frame.Height );
 			View.AddSubview ( sheduleTableViewController.View );
 			
 			base.ViewDidLoad ();
