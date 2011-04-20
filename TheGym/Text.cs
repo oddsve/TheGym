b975811e1b43ae5dc352ab3a26f03447930dbbd3
Odd Morten <odd.morten@sveas.org>
2011-04-20 11:22:25 +0200

    lagt til feilmeldinger på påloggingsfeil og auto til settings hvis du ikke har lagt inn passord

---
+++ TheGym/Text.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace TheGym
+{
+	public static class Text
+	{
+		private static Dictionary<string,string> texts; 
+		public static void initialize ()
+		{
+			texts = new Dictionary<string, string>();
+			texts.Add("Felaktigt anv&#xE4;ndarnamn", "Brukernavnet finnes ikke" );
+			texts.Add( "Felaktigt l&#xF6;senord", "Passordet stemmer ikke" );
+			
+		}
+		
+		public static string getString  ( string key )
+		{
+			if ( texts.ContainsKey( key ) ) return texts[ key ];
+			else 
+			{	
+				System.Console.WriteLine( key ) ;
+				return key;
+			}
+		}
+	}
+}
+
